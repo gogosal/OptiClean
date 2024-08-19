a744ae839ff585721f41559cf4e4d99be6d19d49
gogosal <diogo.salgueirof@gmail.com>
2024-08-19 16:00:12 +0100

    App

---
+++ OptiClean/OptiClean/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class OptiClean
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(OptiClean))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Snow
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.Snow
+        Me.Label1.Name = "Label1"
+        '
+        'Label2
+        '
+        resources.ApplyResources(Me.Label2, "Label2")
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.ForeColor = System.Drawing.Color.Snow
+        Me.Label2.Name = "Label2"
+        '
+        'Label3
+        '
+        resources.ApplyResources(Me.Label3, "Label3")
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.ForeColor = System.Drawing.Color.Snow
+        Me.Label3.Name = "Label3"
+        '
+        'Label4
+        '
+        resources.ApplyResources(Me.Label4, "Label4")
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.ForeColor = System.Drawing.Color.Snow
+        Me.Label4.Name = "Label4"
+        '
+        'Label5
+        '
+        resources.ApplyResources(Me.Label5, "Label5")
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.ForeColor = System.Drawing.Color.Snow
+        Me.Label5.Name = "Label5"
+        '
+        'OptiClean
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.OptiClean.My.Resources.Resources.istockphoto_1169630303_612x612
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "OptiClean"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ OptiClean/OptiClean/Form1.vb
@@ -0,0 +1,168 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+Imports System.Runtime.InteropServices
+Imports System.Management
+Imports System.Security.AccessControl
+
+Public Class OptiClean
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label3.Hide()
+        Label4.Hide()
+
+        Dim path As String = "C:\Windows\Temp"
+
+        For Each deleteFile In Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                File.Delete(deleteFile)
+            Catch ex As Exception
+
+            End Try
+        Next
+        For Each deleteFolder In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                Directory.Delete(deleteFolder)
+            Catch ex As Exception
+
+            End Try
+        Next
+
+        path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "/Temp"
+
+        For Each deleteFile In Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                File.Delete(deleteFile)
+            Catch ex As Exception
+
+            End Try
+        Next
+        For Each deleteFolder In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                Directory.Delete(deleteFolder)
+            Catch ex As Exception
+
+            End Try
+        Next
+
+        path = "C:\Windows\Prefetch"
+
+        For Each deleteFile In Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                File.Delete(deleteFile)
+            Catch ex As Exception
+
+            End Try
+        Next
+        For Each deleteFolder In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                Directory.Delete(deleteFolder)
+            Catch ex As Exception
+
+            End Try
+        Next
+
+        Try
+            Dim IsSuccess As UInteger = SHEmptyRecycleBin(IntPtr.Zero, Nothing, RecycleFlags.SHRB_NOCONFIRMATION)
+        Catch ex As Exception
+
+        End Try
+
+        path = "C:\Windows\Downloaded Program Files"
+
+        For Each deleteFile In Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                File.Delete(deleteFile)
+            Catch ex As Exception
+
+            End Try
+        Next
+        For Each deleteFolder In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                Directory.Delete(deleteFolder)
+            Catch ex As Exception
+
+            End Try
+        Next
+
+        path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\Microsoft\Windows\INetCache\IE"
+
+        For Each deleteFile In Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                File.Delete(deleteFile)
+            Catch ex As Exception
+
+            End Try
+        Next
+        For Each deleteFolder In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                Directory.Delete(deleteFolder)
+            Catch ex As Exception
+
+            End Try
+        Next
+
+        path = "C:\Windows\CbsTemp"
+
+        For Each deleteFile In Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                File.Delete(deleteFile)
+            Catch ex As Exception
+
+            End Try
+        Next
+        For Each deleteFolder In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                Directory.Delete(deleteFolder)
+            Catch ex As Exception
+
+            End Try
+        Next
+
+        path = "C:\Windows\SoftwareDistribution\Download"
+
+        For Each deleteFile In Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                File.Delete(deleteFile)
+            Catch ex As Exception
+
+            End Try
+        Next
+        For Each deleteFolder In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+            Try
+                Directory.Delete(deleteFolder)
+            Catch ex As Exception
+
+            End Try
+        Next
+        Label4.Show()
+    End Sub
+
+    Private Enum RecycleFlags As UInteger
+        SHRB_NOCONFIRMATION = &H1
+        SHRB_NOPROGRESSUI = &H2
+        SHRB_NOSOUND = &H4
+    End Enum
+
+    <DllImport("Shell32.dll", CharSet:=CharSet.Unicode)>
+    Private Shared Function SHEmptyRecycleBin(ByVal hwnd As IntPtr, ByVal pszRootPath As String, ByVal dwFlags As RecycleFlags) As UInteger
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label4.Hide()
+        Label3.Hide()
+        Dim process As New Process()
+        Dim processInfo As New ProcessStartInfo()
+        processInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden
+        processInfo.Verb = "runas"
+        processInfo.FileName = "powershell.exe"
+        processInfo.Arguments = "/C Optimize-Volume -DriveLetter C -ReTrim -Verbose"
+        process.StartInfo = processInfo
+        process.Start()
+        Label3.Show()
+    End Sub
+
+    Private Sub OptiClean_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        Label4.Hide()
+        Label3.Hide()
+    End Sub
+End Class
